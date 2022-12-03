eb32f4b67e185d6dfdc19df8bc684d7214deb270
kalanjiyaraj01@gmail.com <kalanjiyaraj01@gmail.com>
2022-12-03 22:55:57 +0530

    responsive logout

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 function Welcome() {
   return (
-    <div className="container-fluid m-5">
+    <div className="container-lg mt-5">
       <div className="row justify-content-center">
         <div className="col-lg-12 text-center">
           <h1>Welcome</h1>
         </div>
-                      <Link className="btn btn-primary" to={"/"}>
+                      <Link className="btn btn-primary btn-sm" to={"/"}>
                         Logout
                       </Link>
       </div>
