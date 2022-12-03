95ab0087e75fac6bc02a8fd06f47ac02416c2479
kalanjiyaraj01@gmail.com <kalanjiyaraj01@gmail.com>
2022-12-03 22:09:21 +0530

    lg added

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 function Welcome() {
   return (
-    <div className="container m-5">
-      <div className="row">
-        <div className="col-12 text-center">
+    <div className="container-fluid m-5">
+      <div className="row justify-content-center">
+        <div className="col-lg-12 text-center">
           <h1>Welcome</h1>
         </div>
                       <Link className="btn btn-primary" to={"/"}>
